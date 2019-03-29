5bbf20f2815429799ecaf7fb0b597760fc80468c
iecya <federica.delisi@hotmail.it>
2019-03-29 10:38:14 +0000

    Setup server and serve index.html

---
+++ server.js
@@ -0,0 +1,56 @@
+//Load HTTP module
+const http = require("http")
+var fs = require('fs')
+var path = require('path')
+const hostname = '127.0.0.1'
+const port = 3000;
+
+//Create HTTP server and listen on port 3000 for requests
+http.createServer(function(req, response) {
+    var filePath = '.' + req.url;
+    if (filePath === './') {
+        filePath = './index.html';
+    }
+
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.svg': 'application/image/svg+xml'
+    };
+
+    var contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT') {
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end();
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    })
+}).listen(port, hostname, function() {
+    console.log('Server running at http://'+ hostname +':' + port + '/')
+})
